d49c47a9614d100f475b6fcccb905e6a7958ae03
Jenna Deamer <jennadeamer@gmail.com>
2025-12-18 19:44:59 -0500

    add lesson challenges

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -24,6 +24,9 @@ const { body, validationResult, matchedData } = require("express-validator");
 
 const alphaErr = "must only contain letters.";
 const lengthErr = "must be between 1 and 10 characters.";
+const ageErr = "must be between 18 and 120"
+const emailErr = "must be a valid email address"
+const bioErr = 'must be no longer than 200 characters'
 
 const validateUser = [
   body("firstName").trim()
@@ -32,6 +35,13 @@ const validateUser = [
   body("lastName").trim()
     .isAlpha().withMessage(`Last name ${alphaErr}`)
     .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+    body("email").trim()
+    .isEmail().withMessage(`Email ${emailErr}`),
+    body('age').optional({ checkFalsy: true }).trim()
+    .isInt({min: 18, max:120}).withMessage(`Age ${ageErr}`),
+     body('bio').optional({ checkFalsy: true }).trim()
+    .isLength({max:200}).withMessage(`Bio ${bioErr}`),
+
 ];
 
 // We can pass an entire array of middleware validations to our controller.
@@ -45,8 +55,8 @@ exports.usersCreatePost = [
         errors: errors.array(),
       });
     }
-    const { firstName, lastName } = matchedData(req);
-    usersStorage.addUser({ firstName, lastName });
+    const { firstName, lastName, email, age, bio } = matchedData(req);
+    usersStorage.addUser({ firstName, lastName, email, age, bio });
     res.redirect("/");
   }
 ];
@@ -83,3 +93,8 @@ exports.usersDeletePost = (req, res) => {
   usersStorage.deleteUser(req.params.id);
   res.redirect("/");
 };
+
+exports.usersSearchGet = (req,res) =>{
+  const user  =  usersStorage.getUserByEmail(req.query.userEmail);
+    res.render("search", {user})
+}
--- storages/usersStorage.js
+++ storages/usersStorage.js
@@ -6,14 +6,21 @@ class UsersStorage {
     this.id = 0;
   }
 
-  addUser({ firstName, lastName }) {
+  addUser({ firstName, lastName, email, bio, age}) {
     const id = this.id;
-    this.storage[id] = { id, firstName, lastName };
+    this.storage[id] = { id, firstName, lastName, email, bio, age};
     this.id++;
   }
 
   getUsers() {
     return Object.values(this.storage);
+  }
+
+  getUserByEmail(emailToSearch){
+     console.log("Searching for:", emailToSearch);
+  console.log("All users:", Object.values(this.storage));
+    return Object.values(this.storage)
+    .find(user => user.email === emailToSearch);
   }
 
   getUser(id) {
